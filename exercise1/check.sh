2831c024ceb9eed742c36ccc3e8ecb744eada1f3
theabm <andres_bermeo@outlook.com>
2023-09-10 16:32:52 +0200

    working omp v2

--- exercise1/check.sh
+++ exercise1/check.sh
@@ -11,14 +11,22 @@ mv snapshot_00100 snapshot_00100.serial
 ./conway_serial_pgm.v2.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
 mv snapshot_00100 snapshot_00100.serial.v2
 
+export OMP_NUM_THREADS=16
 ./conway_omp_pgm.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
 mv snapshot_00100 snapshot_00100.omp
+
+./conway_omp_pgm.v2.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
+mv snapshot_00100 snapshot_00100.omp.v2
 
 mpirun -np 8 conway_mpi_pgm.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
 mv snapshot_00100 snapshot_00100.mpi
 
-mpirun -np 8 conway_hybrid_pgm.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
+export OMP_NUM_THREADS=4
+export OMP_PLACES=cores
+mpirun --map-by socket -np 2 conway_hybrid_pgm.out -r -f snark_loop_01.pgm -n 100 -s 100 -e 1
 mv snapshot_00100 snapshot_00100.hybrid
+
+export OMP_NUM_THREADS=16
 
 echo "comparing serial and omp"
 cmp snapshot_00100.serial snapshot_00100.omp >&1
@@ -30,6 +38,8 @@ cmp snapshot_00100.mpi snapshot_00100.hybrid >&1
 echo "starting checks for v2"
 echo "comparing serial v1 and serial v2"
 cmp snapshot_00100.serial snapshot_00100.serial.v2 >&1
+echo "comparing omp v1 and omp v2 "
+cmp snapshot_00100.omp snapshot_00100.omp.v2 >&1
 
 rm snapshot_*
 
