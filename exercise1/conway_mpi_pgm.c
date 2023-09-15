fddf9d41dc98de846116e5259f526a11bcef40dd
theabm <andres_bermeo@outlook.com>
2023-09-15 14:12:33 +0200

    reduce to allreduce to fix slurm issue

--- exercise1/conway_mpi_pgm.c
+++ exercise1/conway_mpi_pgm.c
@@ -539,12 +539,9 @@ int main(int argc, char **argv)
         double end_time = MPI_Wtime();
         double local_time = end_time-start_time;
         double global_time_avg;
-        MPI_Reduce(&local_time, &global_time_avg, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        if(rank==0)
-        {
-            global_time_avg/=size;
-            printf("%d,%lf\n",size,global_time_avg);
-        }
+        MPI_Allreduce(&local_time, &global_time_avg, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        global_time_avg/=size;
+        printf("%d,%lf\n",size,global_time_avg);
     
         free(snapshot_name);
         free(header);
@@ -756,12 +753,9 @@ int main(int argc, char **argv)
         double end_time = MPI_Wtime();
         double local_time = end_time-start_time;
         double global_time_avg;
-        MPI_Reduce(&local_time, &global_time_avg, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-        if(rank==0)
-        {
-            global_time_avg/=size;
-            printf("%d,%lf\n",size,global_time_avg);
-        }
+        MPI_Allreduce(&local_time, &global_time_avg, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        global_time_avg/=size;
+        printf("%d,%lf\n",size,global_time_avg);
     
         free(snapshot_name);
         free(header);
