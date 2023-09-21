af100d59700f87a5f07753ff9101d171a27d4ab8
theabm <andres_bermeo@outlook.com>
2023-09-21 21:23:10 +0200

    report

--- exercise1/pattern_pgm/rle_to_pgm_generator.py
+++ exercise1/pattern_pgm/rle_to_pgm_generator.py
@@ -2,11 +2,12 @@ from PIL import Image
 import numpy as np
 
 # Define the size of the grid, generation number, and custom header
-grid_size = 65  # Adjust this based on your desired grid size
+grid_size = 10000  # Adjust this based on your desired grid size
 
 # Define the Snark loop pattern for each generation (in RLE format)
 pgm_patterns = {
-    "snark_loop":"27b2o$27bobo$29bo4b2o$25b4ob2o2bo2bo$25bo2bo3bobob2o$28bobobobo$29b2obobo$33bo2$19b2o$20bo8bo$20bobo5b2o$21b2o$35bo$36bo$34b3o2$25bo$25b2o$24bobo4b2o22bo$31bo21b3o$32b3o17bo$34bo17b2o2$45bo$46b2o12b2o$45b2o14bo$3b2o56bob2o$4bo9b2o37bo5b3o2bo$2bo10bobo37b2o3bo3b2o$2b5o8bo5b2o35b2obo$7bo13bo22b2o15bo$4b3o12bobo21bobo12b3o$3bo15b2o22bo13bo$3bob2o35b2o5bo8b5o$b2o3bo3b2o37bobo10bo$o2b3o5bo37b2o9bo$2obo56b2o$3bo14b2o$3b2o12b2o$19bo2$11b2o17bo$12bo17b3o$9b3o21bo$9bo22b2o4bobo$38b2o$39bo2$28b3o$28bo$29bo$42b2o$35b2o5bobo$35bo8bo$44b2o2$31bo$30bobob2o$30bobobobo$27b2obobo3bo2bo$27bo2bo2b2ob4o$29b2o4bo$35bobo$36b2o!"
+    "snark_loop":"27b2o$27bobo$29bo4b2o$25b4ob2o2bo2bo$25bo2bo3bobob2o$28bobobobo$29b2obobo$33bo2$19b2o$20bo8bo$20bobo5b2o$21b2o$35bo$36bo$34b3o2$25bo$25b2o$24bobo4b2o22bo$31bo21b3o$32b3o17bo$34bo17b2o2$45bo$46b2o12b2o$45b2o14bo$3b2o56bob2o$4bo9b2o37bo5b3o2bo$2bo10bobo37b2o3bo3b2o$2b5o8bo5b2o35b2obo$7bo13bo22b2o15bo$4b3o12bobo21bobo12b3o$3bo15b2o22bo13bo$3bob2o35b2o5bo8b5o$b2o3bo3b2o37bobo10bo$o2b3o5bo37b2o9bo$2obo56b2o$3bo14b2o$3b2o12b2o$19bo2$11b2o17bo$12bo17b3o$9b3o21bo$9bo22b2o4bobo$38b2o$39bo2$28b3o$28bo$29bo$42b2o$35b2o5bobo$35bo8bo$44b2o2$31bo$30bobob2o$30bobobobo$27b2obobo3bo2bo$27bo2bo2b2ob4o$29b2o4bo$35bobo$36b2o!",
+    "glider": "3b1o$1b1o1b1o$2b2o"
 }
 
 # Create a blank PGM image
@@ -59,11 +60,11 @@ def rle_to_grid(rle):
     return grid
 
 # Convert the RLE pattern for the specified generation to a grid
-pattern = pgm_patterns["snark_loop"]
+pattern = pgm_patterns["glider"]
 grid = rle_to_grid(pattern)
 
 # Open the file for writing with a custom header
-with open("snark_loop.pgm", "wb") as f:
+with open("glider_10k.pgm", "wb") as f:
     # Write the PGM header
     header = f"P5 {grid_size} {grid_size} 1\n"
     f.write(header.encode())
